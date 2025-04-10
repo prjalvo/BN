9e3d3f5caf8a090684976aa77f24b7c6cdf31234
Pedro Romano <pedropaulo_ledesma@hotmail.com>
2025-04-10 19:20:40 -0300

    chore: downgrade and change rules eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -21,6 +21,13 @@ module.exports = {
       { vars: 'all', varsIgnorePattern: '^_', args: 'after-used', argsIgnorePattern: '^_' },
     ],
     'no-multiple-empty-lines': ['warn', { max: 1, maxEOF: 0 }],
+    'padding-line-between-statements': [
+      'warn',
+      { blankLine: 'always', prev: '*', next: 'return' },
+      { blankLine: 'always', prev: ['const', 'let', 'var'], next: '*' },
+      { blankLine: 'any', prev: ['const', 'let', 'var'], next: ['const', 'let', 'var'] },
+    ],
+    'no-empty-function': ['warn', { allow: ['arrowFunctions'] }],
 
     // React
     'react/jsx-indent': ['warn', 2],
@@ -35,7 +42,16 @@ module.exports = {
     // Console
     'no-console': ['warn', { allow: ['warn', 'error'] }],
 
+    //typescript
+    '@typescript-eslint/explicit-module-boundary-types': 'warn',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/ban-ts-comment': ['warn', { 'ts-ignore': 'allow-with-description' }],
+    '@typescript-eslint/no-non-null-assertion': 'warn',
+
     // Extra best practices
+    curly: ['error', 'all'],
+    'no-else-return': 'warn',
+    'prefer-const': 'warn',
     eqeqeq: ['error', 'always'],
     'no-shadow': 'warn',
     'sort-imports': [
@@ -44,6 +60,14 @@ module.exports = {
         ignoreDeclarationSort: true,
         ignoreCase: false,
         memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
+      },
+    ],
+    'import/order': [
+      'warn',
+      {
+        groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
+        'newlines-between': 'always',
+        alphabetize: { order: 'asc', caseInsensitive: true },
       },
     ],
   },
